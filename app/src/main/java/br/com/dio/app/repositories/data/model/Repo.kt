6cf8021a1570a94aaa25dfd425f7d47837d9cc06
Punkmic <mattheus_ribeiro@outlook.com>
2022-08-01 13:20:07 -0300

    Add share and open link buttons

--- app/src/main/java/br/com/dio/app/repositories/data/model/Repo.kt
+++ app/src/main/java/br/com/dio/app/repositories/data/model/Repo.kt
@@ -8,7 +8,7 @@ data class Repo(
     val owner: Owner,
     @SerializedName("stargazers_count")
     val stargazersCount: Long,
-    val language: String,
+    val language: String?,
     @SerializedName("html_url")
     val htmlURL: String,
     val description: String
--- app/src/main/java/br/com/dio/app/repositories/ui/RepoListAdapter.kt
+++ app/src/main/java/br/com/dio/app/repositories/ui/RepoListAdapter.kt
@@ -1,13 +1,21 @@
 package br.com.dio.app.repositories.ui
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.BackgroundColorSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import br.com.dio.app.repositories.R
 import br.com.dio.app.repositories.data.model.Repo
 import br.com.dio.app.repositories.databinding.ItemRepoBinding
 import com.bumptech.glide.Glide
+import java.util.*
 
 class RepoListAdapter : ListAdapter<Repo, RepoListAdapter.ViewHolder>(DiffCallback()) {
 
@@ -26,10 +34,40 @@ class RepoListAdapter : ListAdapter<Repo, RepoListAdapter.ViewHolder>(DiffCallba
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Repo) {
+            val context = binding.root.context
             binding.tvRepoName.text = item.name
             binding.tvRepoDescription.text = item.description
-            binding.tvRepoLanguage.text = item.language
             binding.chipStar.text = item.stargazersCount.toString()
+            binding.tvRepoLanguage.apply {
+                item.language?.let {  language ->
+                    val string = SpannableString(language).apply {
+                        val rnd = Random()
+                        val backgroundColor = Color.argb(155, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+                        setSpan(BackgroundColorSpan(backgroundColor), 0, language.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                    }
+                    text = string
+                }
+            }
+            item.language
+
+            binding.btnOpenLink.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(item.htmlURL))
+                context.startActivity(intent)
+            }
+            binding.ivShare.setOnClickListener {
+                val intent = Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_SUBJECT, item.name)
+                    putExtra(Intent.EXTRA_TEXT, item.htmlURL)
+                }
+                context.startActivity(
+                    Intent.createChooser(
+                        intent,
+                        context.getString(R.string.open_link_with)
+                    )
+                )
+
+            }
 
             Glide.with(binding.root.context)
                 .load(item.owner.avatarURL).into(binding.ivOwner)
